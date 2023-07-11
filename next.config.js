54b6c5dc2ac9d9daf52ed204c2c1002f5d7ab85f
Alisa Treyfeld <alisa.tr@gmail.com>
2023-07-11 12:05:45 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,28 @@
 const nextConfig = {
     reactStrictMode: true,
     transpilePackages: ["three, react-three-fiber", "drei"],
+    images: {
+        domains: [
+            "avatars.githubusercontent.com",
+            "faderoom-headless.us",
+            "www.faderoom-headless.us",
+            "dtmqnbkq3btfh.cloudfront.net",
+            "secure.gravatar.com",
+            "automattic.com",
+            "serve.onegraph.com",
+            "onegraph.com",
+            "maps.google.com",
+            "lh3.googleusercontent.com",
+            "maps.gstatic.com",
+            "thefaderoom146.booksy.com",
+            "dev-3cqt2bq0.auth0.com",
+            "scontent-sea1-1.xx.fbcdn.net",
+            "d2zdpiztbgorvt.cloudfront.net",
+            "platform-lookaside.fbsbx.com",
+            "square-postoffice-production.s3.amazonaws.com",
+            "media.giphy.com",
+        ],
+    },
     module: {
         rules: [
             {
@@ -18,7 +40,7 @@ const nextConfig = {
             {
                 test: /\.(mp4|mov)$/,
                 loader: "file-loader",
-            }
+            },
         ],
     },
 };
