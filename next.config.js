05e02d8b90e35100ccbdc5627f25b95d1e8df181
Alisa Treyfeld <alisa.tr@gmail.com>
2023-07-11 14:53:42 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,20 +7,6 @@ const withVideos = require('next-videos')
 const nextConfig = {
     reactStrictMode: true,
     transpilePackages: ["three, react-three-fiber", "drei"],
-    module: {
-        rules: [
-            {
-                test: /\.svg$/,
-                loader: "svg-inline-loader",
-            },
-            {
-                  test: /\.(glb|gltf)$/,
-                  use: {
-                    loader: "file-loader",
-                  },
-            }
-                ],
-    },
 };
 
 
