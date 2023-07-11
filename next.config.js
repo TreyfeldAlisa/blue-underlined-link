08697edd54b836dfb46bce937c856e0093ece98f
Alisa Treyfeld <alisa.tr@gmail.com>
2023-07-11 14:13:53 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,47 +2,37 @@
  * @type {import('next').NextConfig}
  * */
 
-const nextConfig = {
+const withTM = require("next-transpile-modules")([
+  "three/examples/jsm/lines/LineGeometry",
+  "@react-three/drei",
+     "@react-three/fiber",
+"three, react-three-fiber", "drei"
+])
+
+const nextConfig = withTM({
     reactStrictMode: true,
     transpilePackages: ["three, react-three-fiber", "drei"],
-    images: {
-        domains: [
-            "avatars.githubusercontent.com",
-            "faderoom-headless.us",
-            "www.faderoom-headless.us",
-            "dtmqnbkq3btfh.cloudfront.net",
-            "secure.gravatar.com",
-            "automattic.com",
-            "serve.onegraph.com",
-            "onegraph.com",
-            "maps.google.com",
-            "lh3.googleusercontent.com",
-            "maps.gstatic.com",
-            "thefaderoom146.booksy.com",
-            "dev-3cqt2bq0.auth0.com",
-            "scontent-sea1-1.xx.fbcdn.net",
-            "d2zdpiztbgorvt.cloudfront.net",
-            "platform-lookaside.fbsbx.com",
-            "square-postoffice-production.s3.amazonaws.com",
-            "media.giphy.com",
-        ],
-    },
     module: {
         rules: [
             {
                 test: /\.svg$/,
                 loader: "svg-inline-loader",
             },
+{
+      test: /\.(glb|gltf)$/,
+      use: {
+        loader: "file-loader",
+      },
+    }
             {
-                test: /\.(glb|gltf)$/,
+                test: /\.(mp4|webm|ogg|swf|ogv)$/,
                 loader: "file-loader",
             },
             {
-                test: /\.(mp4|mov)$/,
+                test: /\.gif$/,
                 loader: "file-loader",
-            },
         ],
     },
-};
+});
 
 module.exports = nextConfig;
