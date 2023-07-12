4d097cac9a193a660e461d93f58c79e26d420107
Alisa Treyfeld <alisa.tr@gmail.com>
2023-07-12 12:25:04 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withImages = require("next-images");
 // const with3D = require("next-transpile-modules")(["three", "@react-three/fiber", "@react-three/drei"])
 
 const nextConfig = {
-    assetPrefix: !debug ? 'https://treyfeldalisa.github.io/blue-underlined-link/' : '',
+    assetPrefix: 'https://treyfeldalisa.github.io/blue-underlined-link/',
     reactStrictMode: true,
     transpilePackages: ["@react-three-fiber", "@rect-three-drei"],
     images: {
