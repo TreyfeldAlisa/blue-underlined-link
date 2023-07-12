2e2b5acd9e008f2ed1d5ee8dab28097a3ab4ef0a
Alisa Treyfeld <alisa.tr@gmail.com>
2023-07-12 11:54:32 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ const nextConfig = {
     transpilePackages: ["@react-three-fiber", "@rect-three-drei"],
     images: {
         disableStaticImages: true,
+        unoptimized: true,
     },
 };
 
