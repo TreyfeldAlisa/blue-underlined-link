57b60152576c23e7aa58247ecb6c19738883eb8c
Alisa Treyfeld <alisa.tr@gmail.com>
2023-07-17 17:39:24 +0300

    Update Categories.jsx

--- views/Categories.jsx
+++ views/Categories.jsx
@@ -1,9 +1,10 @@
+
+import React from "react";
 import Background from "@/components/Background/Background";
 import Footer from "@/components/Footer/Footer";
 import LetsTalk from "@/components/Works/LetsTalk";
 import Works from "@/components/Works/Works";
-import React from "react";
-import styles from "../components/Background/Background.module.css";
+import styles from "@/components/Background/Background.module.css";
 import cn from "classnames";
 import Navbar from "@/components/Navbar/NavbarCategory";
 
