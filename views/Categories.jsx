9d623749203dab1b88f1c43051819ed658c92ee9
Alisa Treyfeld <alisa.tr@gmail.com>
2023-07-25 16:17:57 +0300

    Update Categories.jsx

--- views/Categories.jsx
+++ views/Categories.jsx
@@ -1,30 +1,13 @@
-
+import CategoryCard from "@/components/CategoryCard";
+import CategoriesLayout from "@/layouts/CategoriesLayout";
 import React from "react";
-import Background from "@/components/Background/Background";
-import Footer from "@/components/Footer/Footer";
-import LetsTalk from "@/components/Works/LetsTalk";
-import Works from "@/components/Works/Works";
-import styles from "@/components/Background/Background.module.css";
-import cn from "classnames";
-import Navbar from "@/components/Navbar/NavbarCategory";
+
 
 const Categories = () => {
   return (
-    <div className="allworks">
-      <div className={styles.videoContainer}>
-        <video
-          controls
-          autoPlay
-          playsInline
-          loop
-          muted
-          className={cn(styles.video, "embed-responsive-item")}
-          src={require("../public/video/ASCII.mp4")}
-        />
-      </div>
-      <Navbar />
-      <Footer />
-    </div>
+    <CategoriesLayout shouldBeWithLinks={true}>
+      <CategoryCard/>
+    </CategoriesLayout>
   );
 };
 
