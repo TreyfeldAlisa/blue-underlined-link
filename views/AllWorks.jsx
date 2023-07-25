ffd9e639e14a4d2078bf8204ce9a6c61c9595377
Alisa Treyfeld <alisa.tr@gmail.com>
2023-07-25 15:30:24 +0300

    Update AllWorks.jsx

--- views/AllWorks.jsx
+++ views/AllWorks.jsx
@@ -11,7 +11,7 @@ export default function AllWorks() {
       <Background />
         <NavbarAllWorks shouldBeWithLinks />
         <Works />
-        {/* <LetsTalk /> */}
+        <LetsTalk />
         <Footer />
     </div>
   );
